7e8be79c78e1ab33136f2f97e93fc6b46ec01de7
MikeBarskiy <barmik95@yandex.ru>
2016-03-26 13:46:11 +0300

    Change AuthController Delete spring-security

--- src/main/java/com/netcracker/tripnwalk/controller/AuthController.java
+++ src/main/java/com/netcracker/tripnwalk/controller/AuthController.java
@@ -1,29 +1,37 @@
 package com.netcracker.tripnwalk.controller;
 
+import com.netcracker.tripnwalk.entry.Itineraries;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-class AuthController {
+@Controller
+public class AuthController {
     private String client_id = "5368462";
     private String redirect_uri = "http://localhost:9095/";
     private String display = "popup";
     private String response_type = "token";
     private String access_token;
 
-    AuthController() throws IOException {
-        HttpClient httpClient = new DefaultHttpClient();
+    @RequestMapping(value = "/auth", method = RequestMethod.GET)
+    public void auth() throws IOException {
+//        HttpClient httpClient = new DefaultHttpClient();
         String reqUrl = "http://oauth.vk.com/authorize?" +
                 "client_id=" + client_id +
                 "&redirect_uri=" + redirect_uri +
                 "&display=" + display +
                 "&response_type=" + response_type;
-        HttpResponse response = null;
+//        HttpResponse response = null;
 
         try {
             Desktop.getDesktop().browse(new URL(reqUrl).toURI());
