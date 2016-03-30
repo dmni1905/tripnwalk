daa99f029c145a37a5784504ce0b7ba2482564e3
pr1st0n <k0t9p@yahoo.com>
2016-03-30 22:24:41 +0300

    Added redirect for vk auth on client.

--- client/static/js/controllers/user-controller.js
+++ client/static/js/controllers/user-controller.js
@@ -2,10 +2,11 @@
 
 app.controller('UserCtrl', ['$scope', 'UserService', function($scope, AuthService) {
   var authorize = () => {
-    AuthService.authorize()
-      .then((res, err, info) => {
-        res;
-      });
+    window.location.href = "http://oauth.vk.com/authorize?" +
+      "client_id=5368462" +
+      "&redirect_uri=http://localhost/docs/tmp/client/index.html" +
+      "&display=popup" +
+      "&response_type=token";
   };
 
   $scope.authorize = authorize;
