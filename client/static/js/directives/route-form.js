41c6d073b4fcd144866f5a99c68e5f3878da1e24
pr1st0n <k0t9p@yahoo.com>
2016-04-05 23:49:41 +0300

    In-progress commit for client add/edit route section.

--- client/static/js/directives/route-form.js
+++ client/static/js/directives/route-form.js
@@ -3,21 +3,33 @@
 app.directive('addRouteForm', function() {
   return {
     restrict: 'E',
-    template: '<button route-form>Add route</button>'
+    template: '<label type="button" class="add-route btn btn-primary" route-form>ADD ROUTE</label>'
   };
 });
 
 app.directive('routeForm', function($compile, $templateRequest) {
   return {
-    link: function(scope, element, attrs){
+    link: function($scope, element, attrs){
       //TODO look at controllers to handle route-form.
       element.bind('click', function(evt){
-        var route = _.find(scope.routes, route => {
-          return route.id == evt.target.getAttribute('class').match(/id-(.+)/)[1];
-        });
+        $scope.curRoute = undefined;
+
+        if (_.contains(evt.target.getAttribute('class'), 'id-')) {
+          $scope.curRoute =  _.find($scope.routes, route => {
+            return route.id == evt.target.getAttribute('class').match(/id-(.+)/)[1];
+          });
+
+          $scope.curRoute.points = _.sortBy($scope.curRoute.points, point => point.position);
+        }
 
         $templateRequest('templates/route-form.html').then(html => {
-          angular.element($('.route-form-place')).append($compile(html)(scope));
+          angular.element($('.route-form-place')).append($compile(html)($scope));
+
+          $('#clear-route').on('click', () => {
+            $('.route-form-place').empty();
+
+            $scope.clearRoute();
+          });
         });
       });
     }
