23bdf164d76f73151ce03fbc5bf7bc005dcb3942
pr1st0n <k0t9p@yahoo.com>
2016-04-08 01:23:35 +0300

    Removed lib directory. Added vk auth session initializer.

--- client/static/js/controllers/user-controller.js
+++ client/static/js/controllers/user-controller.js
@@ -1,11 +1,23 @@
 'use strict';
 
-app.controller('UserCtrl', ['$scope', '$window', function($scope, w) {
+app.controller('UserCtrl', function($scope, UserService) {
+  function getTokenFromUrl() {
+    var tokenObj = {};
+
+    _.each(['access_token', 'expires_in', 'user_id'], param => {
+      tokenObj[param] = new RegExp('[\\?#&]' + param.replace(/[\[]/,'\\\[').replace(/[\]]/,'\\\]') + '=([^&#]*)')
+        .exec(location.href)[1];
+    });
+
+    return tokenObj;
+  }
+
+  _.contains(window.location.href, '#access_token=') && UserService.getSession(getTokenFromUrl());
+
   $scope.authorize = function () {
       window.location.href = 'http://oauth.vk.com/authorize?' +
             'client_id=5368462' +
-            //'&redirect_uri=http://localhost/docs/tmp/client/index.html' +
-            '&redirect_uri=http://localhost:63342/trip-n-walk/client/auth.html' +
+            '&redirect_uri=http://localhost/docs/tmp/client/index.html' +
             '&display=popup' +
             '&response_type=token';
   };
@@ -14,4 +26,4 @@ app.controller('UserCtrl', ['$scope', '$window', function($scope, w) {
   $scope.bday = '16/11/93';
   $scope.country = 'Russia';
   $scope.email = 'desiresdesigner@gmail.com';
-}]);
+});
--- client/static/js/services/user-service.js
+++ client/static/js/services/user-service.js
@@ -2,14 +2,20 @@
 
 app.factory('UserService', ['$http', '$q', function($http, $q) {
   return {
-    authorize: () => {
-      return $http.get('http://localhost:9095/auth')
+    getSession: (tokenObj) => {
+      return $http.get('http://localhost:9095/session', tokenObj)
         .then(res => {
-          res;
+          //TODO handle session id to Local Storage.
+            console.log('Authorized!');
+
+            window.history.back(1);
         },
         err => {
           //TODO Handle unsuccessful auth.
           err;
+          console.log('Authorization failed!');
+
+          window.history.back(1);
         });
     }
   };
