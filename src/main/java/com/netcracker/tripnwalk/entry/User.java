a8e95949d66c0f83e3d6a9ab7cd0618c1390b577
pr1st0n <k0t9p@yahoo.com>
2016-04-17 23:31:42 +0300

    Fixed warning.

--- src/main/java/com/netcracker/tripnwalk/entry/User.java
+++ src/main/java/com/netcracker/tripnwalk/entry/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.sql.Date;
 import java.util.HashSet;
 import java.util.Set;
 
