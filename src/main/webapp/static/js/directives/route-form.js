8144490d7a7b36e3d3136d0c8f7d69d4c13cbcf6
Дмитрий <dmi.nichik@gmail.com>
2016-05-08 20:09:43 +0300

    upload css

--- src/main/webapp/static/js/directives/route-form.js
+++ src/main/webapp/static/js/directives/route-form.js
@@ -52,3 +52,49 @@ app.directive('routeForm', function($compile, $templateRequest) {
     }
   };
 });
+
+  app.directive('routeGuestForm', function($compile, $templateRequest) {
+    return {
+      link: function($scope, element, attrs){
+        //TODO look at controllers to handle route-form.
+        element.bind('click', function(evt){
+          $scope.removeForm = () => {
+            //TODO proper clean-up.
+            $('.route-form-place').empty();
+            $scope.clearRoute();
+
+            $scope.toggleRouteMode(false);
+          };
+
+          $('#route-edit-window').length > 0 && $scope.removeForm();
+          $scope.toggleRouteMode(true);
+
+          var existingRoute = evt.target.closest('.route-menu-controls').getAttribute('class');
+
+          if (_.contains(existingRoute, 'id-')) {
+            $scope.curRoute =  _.find($scope.routes, route => {
+              return route.id == existingRoute.match(/id-(.+)/)[1];
+            });
+
+            $scope.curRoute.points = _.sortBy($scope.curRoute.points, point => point.position);
+          }
+
+          $scope.removePoint = $event => {
+            if ($scope.curRoute.points.length <= 2) return;
+
+            var index = $('.panel-group .panel').index($event.currentTarget.closest('.panel'));
+
+            $scope.removePoint0($scope.curRoute.id, index);
+          };
+
+          $templateRequest('/templates/route-guest-form.html').then(html => {
+            angular.element($('.route-form-place')).append($compile(html)($scope));
+
+            $('.js-route-form').draggable();
+
+            $('#clear-route').on('click', () => $scope.removeForm());
+          });
+        });
+      }
+    };
+  });
