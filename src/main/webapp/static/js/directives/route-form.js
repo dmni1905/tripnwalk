fb4e2b20de2d37dbaa8499fbc863c9c2e9b8bda9
Дмитрий <dmi.nichik@gmail.com>
2016-05-17 20:30:43 +0300

    rote-form

--- src/main/webapp/static/js/directives/route-form.js
+++ src/main/webapp/static/js/directives/route-form.js
@@ -55,50 +55,50 @@ app.directive('routeForm', function($compile, $templateRequest) {
   };
 });
 
-  app.directive('routeGuestForm', function($compile, $templateRequest) {
-    return {
-      link: function($scope, element, attrs){
-        //TODO look at controllers to handle route-form.
-        element.bind('click', function(evt){
-          $scope.removeForm = () => {
-            $('.route-form-place').empty();
-            $scope.clearRoute();
+app.directive('routeGuestForm', function($compile, $templateRequest) {
+  return {
+    link: function($scope, element, attrs){
+      //TODO look at controllers to handle route-form.
+      element.bind('click', function(evt){
+        $scope.removeForm = () => {
+          $('.route-form-place').empty();
+          $scope.clearRoute();
 
-            $scope.toggleRouteMode(false);
-            $scope.toggleRouteSelected(false, $scope.curRoute);
-          };
+          $scope.toggleRouteMode(false);
+          $scope.toggleRouteSelected(false, $scope.curRoute);
+        };
 
-          $('#route-edit-window').length > 0 && $scope.removeForm();
-          $scope.toggleRouteMode(true);
+        $('#route-edit-window').length > 0 && $scope.removeForm();
+        $scope.toggleRouteMode(true);
 
-          var existingRoute = evt.target.closest('.route-menu-controls').getAttribute('class');
+        var existingRoute = evt.target.closest('.route-menu-controls').getAttribute('class');
 
-          if (_.contains(existingRoute, 'id-')) {
-            $scope.curRoute =  _.find($scope.routes, route => {
-              return route.id == existingRoute.match(/id-(.+)/)[1];
-            });
-
-            $scope.toggleRouteSelected(true, $scope.curRoute);
-
-            $scope.curRoute.points = _.sortBy($scope.curRoute.points, point => point.position);
-          }
-
-          $scope.removePoint = $event => {
-            if ($scope.curRoute.points.length <= 2) return;
-
-            var index = $('.panel-group .panel').index($event.currentTarget.closest('.panel'));
-
-            $scope.curRoute = $scope.removePoint0($scope.curRoute, index);
-          };
-
-          $templateRequest('/templates/route-guest-form.html').then(html => {
-            angular.element($('.route-form-place')).append($compile(html)($scope));
-
-            $('.js-route-form').draggable();
-
-            $('#clear-route').on('click', () => $scope.removeForm());
+        if (_.contains(existingRoute, 'id-')) {
+          $scope.curRoute =  _.find($scope.routes, route => {
+            return route.id == existingRoute.match(/id-(.+)/)[1];
           });
+
+          $scope.toggleRouteSelected(true, $scope.curRoute);
+
+          $scope.curRoute.points = _.sortBy($scope.curRoute.points, point => point.position);
+        }
+
+        $scope.removePoint = $event => {
+          if ($scope.curRoute.points.length <= 2) return;
+
+          var index = $('.panel-group .panel').index($event.currentTarget.closest('.panel'));
+
+          $scope.removePoint0($scope.curRoute.id, index);
+        };
+
+        $templateRequest('/templates/route-guest-form.html').then(html => {
+          angular.element($('.route-form-place')).append($compile(html)($scope));
+
+          $('.js-route-form').draggable();
+
+          $('#clear-route').on('click', () => $scope.removeForm());
         });
-      }
-    };
-  });
+      });
+    }
+  };
+});
